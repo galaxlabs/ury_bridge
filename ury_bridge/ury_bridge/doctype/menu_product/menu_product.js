662484d20966da0ad614d14251aa536502d6e7e4
galaxlabs <galaxylab2020@gmail.com>
2026-04-01 18:06:10 +0000

    doctype added

---
+++ ury_bridge/ury_bridge/doctype/menu_product/menu_product.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Galaxy Labs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Menu Product", {
+// 	refresh(frm) {
+
+// 	},
+// });
