bbfc23af47eaaf92e2427216620641e53c7cf456
galaxlabs <galaxylab2020@gmail.com>
2026-04-01 15:19:11 +0000

    init

---
+++ ury_bridge/ury_bridge/doctype/ury_website_settings/ury_website_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Galaxy Labs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ury Website Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ury_bridge/ury_bridge/doctype/ury_website_settings/ury_website_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Galaxy Labs and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class UryWebsiteSettings(Document):
+	pass
