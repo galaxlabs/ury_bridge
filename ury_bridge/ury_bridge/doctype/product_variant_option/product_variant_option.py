220b351b669baeac3c1a50cb944d006b6596076a
galaxlabs <galaxylab2020@gmail.com>
2026-04-02 12:01:20 +0000

    deleted variant

--- ury_bridge/ury_bridge/doctype/product_variant_option/product_variant_option.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Galaxy Labs and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ProductVariantOption(Document):
-	pass
--- ury_bridge/ury_bridge/doctype/product_variant_option/test_product_variant_option.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Galaxy Labs and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestProductVariantOption(FrappeTestCase):
-	pass
